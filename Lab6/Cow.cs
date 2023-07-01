3f35d28e28bb9844a7bfd1a9881a6ea24af1ea77
godgold11@gmail.com <godgold11@gmail.com>
2023-07-01 06:15:17 +0300

    Day1

---
+++ Lab6/Cow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class Cow : Mammals
+    {
+
+
+        public Cow(int height, int width, bool MilkFeed) : base(height, width, MilkFeed)
+        {
+        }
+
+        
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Moo Moo");
+        }
+
+
+        public override void Eat()
+        {
+            Console.WriteLine("Cows eats grass and Vegtables ");
+        }
+
+
+
+    }
+}
---
+++ Lab6/Mammals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public abstract class Mammals : Animal
+    {
+        public bool MilkFeed { get; set; }
+
+
+
+        public Mammals(int height, int width,bool MilkFeed) : base(height, width)
+        {
+            MilkFeed = MilkFeed;
+        }
+
+
+        public override void Eat()
+        {
+            Console.WriteLine("this animal eats !");
+        }
+
+    }
+}
